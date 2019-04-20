61d5e0275ba691f6eb8b632e59a6423d10a44e61
Ravikiran Butti <ravikiran763@gmail.com>
2019-04-20 22:17:59 +0530

    Added assembly plugin

--- src/main/java/com/guidetogalaxy/galacticunitconverter/validator/GalacticUnitConverterValidator.java
+++ src/main/java/com/guidetogalaxy/galacticunitconverter/validator/GalacticUnitConverterValidator.java
@@ -6,87 +6,92 @@ import java.util.Set;
 import com.guidetogalaxy.galacticunitconverter.domain.RomanNumber;
 
 /**
- * @program: guide-to-the-galaxy
- * @description: RomanValidator
- * @author: smallsoup
- * @create: 2018-11-15 23:37
- **/
+ * Validator utility class for GalacticUnitConverter application
+ * 
+ * @author Ravikiran Butti
+ *
+ */
+public final class GalacticUnitConverterValidator {
 
-public class GalacticUnitConverterValidator {
+	private GalacticUnitConverterValidator() {
 
-    private GalacticUnitConverterValidator() {
+	}
 
-    }
+	/**
+	 * The symbols "I", "X", "C", and "M" can be repeated three times in succession,
+	 * but no more
+	 */
+	private static final Set<Character> THREE_TIMES_REPEATED_CHARACTERS = new HashSet<>();
 
-    /**
-     * The symbols "I", "X", "C", and "M" can be repeated three times in succession, but no more
-     */
-    private static final Set<Character> THREE_TIMES_REPEATED_CHARACTERS = new HashSet<>();
+	/**
+	 * "V", "L", and "D" can never be subtracted.
+	 */
+	private static final Set<Character> NOT_SUBTRACTED_CHARACTERS = new HashSet<>();
 
-    /**
-     * "V", "L", and "D" can never be subtracted.
-     */
-    private static final Set<Character> NOT_SUBTRACTED_CHARACTERS = new HashSet<>();
+	static {
+		THREE_TIMES_REPEATED_CHARACTERS.add('I');
+		THREE_TIMES_REPEATED_CHARACTERS.add('X');
+		THREE_TIMES_REPEATED_CHARACTERS.add('C');
+		THREE_TIMES_REPEATED_CHARACTERS.add('M');
 
-    static {
-        THREE_TIMES_REPEATED_CHARACTERS.add('I');
-        THREE_TIMES_REPEATED_CHARACTERS.add('X');
-        THREE_TIMES_REPEATED_CHARACTERS.add('C');
-        THREE_TIMES_REPEATED_CHARACTERS.add('M');
+		NOT_SUBTRACTED_CHARACTERS.add('V');
+		NOT_SUBTRACTED_CHARACTERS.add('L');
+		NOT_SUBTRACTED_CHARACTERS.add('D');
+		NOT_SUBTRACTED_CHARACTERS.add('M');
+	}
 
-        NOT_SUBTRACTED_CHARACTERS.add('V');
-        NOT_SUBTRACTED_CHARACTERS.add('L');
-        NOT_SUBTRACTED_CHARACTERS.add('D');
-        NOT_SUBTRACTED_CHARACTERS.add('M');
-    }
+	/**
+	 * Validate the input RomanNumber and return it's integer value if valid, else
+	 * return -1
+	 */
+	public static int validateRoman(String romanNumber) throws Exception {
+		char[] charArray = romanNumber.toCharArray();
+		char previousChar = ' ';
 
-    /**
-     * Validate the input RomanNumber and return it's integer value if valid, else return -1
-     */
-    public static int validateRoman(String romanNumber) throws Exception {
-        char[] charArray = romanNumber.toCharArray();
-        char previousChar = ' ';
+		int characterRepeatCount = 1;
+		int total = 0;
+		int previousCharacterOrdinal = Integer.MAX_VALUE;
+		int currentCharacterOrdinal;
 
-        int characterRepeatCount = 1;
-        int total = 0;
-        int previousCharacterOrdinal = Integer.MAX_VALUE;
-        int currentCharacterOrdinal;
+		for (int i = 0; i < charArray.length; i++) {
+			char currentChar = charArray[i];
+			int currentRomanCharNumericValue = RomanNumber.valueOf(String.valueOf(currentChar)).getValue();
 
-        for (int i = 0; i < charArray.length; i++) {
-            char currentChar = charArray[i];
-            int currentRomanCharNumericValue = RomanNumber.valueOf(String.valueOf(currentChar)).getValue();
+			if (previousChar != ' ') {
+				previousCharacterOrdinal = RomanNumber.valueOf(String.valueOf(previousChar)).ordinal();
+			}
+			currentCharacterOrdinal = RomanNumber.valueOf(String.valueOf(currentChar)).ordinal();
 
-            if (previousChar != ' ') {
-                previousCharacterOrdinal = RomanNumber.valueOf(String.valueOf(previousChar)).ordinal();
-            }
-            currentCharacterOrdinal = RomanNumber.valueOf(String.valueOf(currentChar)).ordinal();
-
-            //They may appear four times if the third and fourth are separated by a smaller value, such as XXXIX
-            if (currentChar == previousChar && ++characterRepeatCount < 4 && THREE_TIMES_REPEATED_CHARACTERS.contains(currentChar)) {
-                total += currentRomanCharNumericValue;
-            } else if (characterRepeatCount > 3) {
-                total = -1;
-            } else if (currentChar == previousChar && !THREE_TIMES_REPEATED_CHARACTERS.contains(currentChar)) {
-                total = -1;
-            } else if (previousCharacterOrdinal < currentCharacterOrdinal && !NOT_SUBTRACTED_CHARACTERS.contains(previousChar)) {
-                int previousRomanCharNumericValue = RomanNumber.valueOf(String.valueOf(previousChar)).getValue();
-                if (previousCharacterOrdinal + 2 >= currentCharacterOrdinal) {
-                    total += currentRomanCharNumericValue - (2 * previousRomanCharNumericValue);
-                    characterRepeatCount = 1;
-                } else {
-                    total = -1;
-                }
-            } else if (previousCharacterOrdinal < currentCharacterOrdinal && NOT_SUBTRACTED_CHARACTERS.contains(previousChar)) {
-                total = -1;
-            } else {
-                characterRepeatCount = 1;
-                total += currentRomanCharNumericValue;
-            }
-            previousChar = currentChar;
-            if (total == -1) {
-                break;
-            }
-        }
-        return total;
-    }
+			// They may appear four times if the third and fourth are separated by a smaller
+			// value, such as XXXIX
+			if (currentChar == previousChar && ++characterRepeatCount < 4
+					&& THREE_TIMES_REPEATED_CHARACTERS.contains(currentChar)) {
+				total += currentRomanCharNumericValue;
+			} else if (characterRepeatCount > 3) {
+				total = -1;
+			} else if (currentChar == previousChar && !THREE_TIMES_REPEATED_CHARACTERS.contains(currentChar)) {
+				total = -1;
+			} else if (previousCharacterOrdinal < currentCharacterOrdinal
+					&& !NOT_SUBTRACTED_CHARACTERS.contains(previousChar)) {
+				int previousRomanCharNumericValue = RomanNumber.valueOf(String.valueOf(previousChar)).getValue();
+				if (previousCharacterOrdinal + 2 >= currentCharacterOrdinal) {
+					total += currentRomanCharNumericValue - (2 * previousRomanCharNumericValue);
+					characterRepeatCount = 1;
+				} else {
+					total = -1;
+				}
+			} else if (previousCharacterOrdinal < currentCharacterOrdinal
+					&& NOT_SUBTRACTED_CHARACTERS.contains(previousChar)) {
+				total = -1;
+			} else {
+				characterRepeatCount = 1;
+				total += currentRomanCharNumericValue;
+			}
+			previousChar = currentChar;
+			if (total == -1) {
+				break;
+			}
+		}
+		return total;
+	}
 }
---
+++ src/test/java/com/guidetogalaxy/galacticunitconverter/app/GalaticUnitConverterAppTest.java
@@ -0,0 +1,12 @@
+package com.guidetogalaxy.galacticunitconverter.app;
+
+import org.junit.Test;
+
+public class GalaticUnitConverterAppTest {
+
+    @Test
+    public void testMain(){
+        String[] args = {};
+        GalacticUnitConverterApp.main(args);
+    }
+}
